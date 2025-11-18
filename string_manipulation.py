f8f605ab834553efe9bf03cbc8a853e65433cbd9
Cebisile16 <ngubsceby@gmail.com>
2025-11-18 14:08:51 +0200

    Update string_manipulation.py

--- string_manipulation.py
+++ string_manipulation.py
@@ -2,41 +2,44 @@
 # --- Easy ---
 
 def to_uppercase(input_string):
-    """Converts a string to uppercase."""
-    pass
+    
+    return to_uppercase("hello")
+print(to_uppercase.upper())
+    
 
 def to_lowercase(input_string):
-    """Converts a string to lowercase."""
-    pass
+    return to_lowercase("HELLO")
+
+print(to_lowercase.lower())
 
 def count_char(input_string, char_to_count):
-    """Counts the number of times a character appears in a string."""
-    pass
+    return( "hello", "l")
+
+print (count_char)
 
 # --- Medium ---
 
 def reverse_string(input_string):
-    """Reverses a string."""
-    pass
+    input_string= "Hello"
+ 
+    return reverse_string[::-1]
+print(reverse_string)
 
+
+    
 def reverse_sentence(input_string):
-    """Reverses the order of words in a sentence."""
-    pass
+   
+
 
 def is_palindrome(input_string):
-    """
-    Checks if a string is a palindrome.
-    Should be case-insensitive and ignore spaces/punctuation.
-    """
+    
     pass
 
 # --- Hard ---
 
 def is_anagram(string1, string2):
     """
-    Checks if two strings are anagrams.
-    Should be case-insensitive and ignore spaces.
-    """
+  #use .sorted()
     pass
 
 def format_date(date_string):
